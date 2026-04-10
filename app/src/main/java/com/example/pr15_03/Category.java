35c9469fa88b99902d2ffb90b92906fe12d85efa
dima <balecored@gmail.com>
2026-04-10 13:05:53 +0500

    активити популар. шаповалов.

---
+++ app/src/main/java/com/example/pr15_03/Category.java
@@ -0,0 +1,11 @@
+package com.example.pr15_03;
+
+public class Category {
+    public int Id;
+    public String Name;
+    public Boolean Active = false;
+    public Category(int id, String name) {
+        this.Id = id;
+        this.Name = name;
+    }
+}
